5359707c18696e31cd5d68105a9c354117c1c6a2
Alexandru Bita <bita.alexandru24@gmail.com>
2025-09-09 23:43:34 +0300

    work work work

--- src/main/scala/agents/OrchestratorAgent.scala
+++ src/main/scala/agents/OrchestratorAgent.scala
@@ -1,12 +1,17 @@
 package alex.demo
 package agents
 
-import agents.Agent.{Command, CommandProps, askLlm}
+import agents.Agent.{Command, CommandProps, askLlm, makeRequestWithRetries}
 
 import org.apache.pekko.actor.typed.Behavior
 import org.apache.pekko.actor.typed.scaladsl.{ActorContext, Behaviors}
+import sttp.client4.quick.*
 
 object OrchestratorAgent extends Agent:
+  private val serverBaseUrl = dotenv.get("SERVER_BASE_URL")
+  private val serverPort = dotenv.get("SERVER_PORT")
+  private val employeeDocsEndpoint = dotenv.get("EMPLOYEE_DOCS_ENDPOINT")
+
   def doStart(systemPrompt: Option[String], commandProps: CommandProps)
     (using context: ActorContext[Command]): Behavior[Command] =
     systemPrompt.foreach(context.log.info)
@@ -34,6 +39,15 @@ object OrchestratorAgent extends Agent:
     (using context: ActorContext[Command]): Behavior[Command] =
     Behaviors.same
   end doCallTool
+
+  private def callDeleteEmployeeDocsTool(secret: String, filename: String, document: String)
+    (using context: ActorContext[Command]): Option[String] =
+    val request = quickRequest
+      .delete(uri"$serverBaseUrl:$serverPort/$employeeDocsEndpoint?secret=$secret")
+      .header("Content-Type", "text/html")
+      .body(document.trim)
+    makeRequestWithRetries(request)
+  end callDeleteEmployeeDocsTool
 
 end OrchestratorAgent
 
--- src/main/scala/agents/WorkerAgent.scala
+++ src/main/scala/agents/WorkerAgent.scala
@@ -10,11 +10,11 @@ import sttp.client4.quick.*
 object WorkerAgent extends Agent:
   private val serverBaseUrl = dotenv.get("SERVER_BASE_URL")
   private val serverPort = dotenv.get("SERVER_PORT")
-  private val getEmployeeCvEndpoint = dotenv.get("GET_EMPLOYEE_CV_ENDPOINT")
-  private val getEmployeeIdEndpoint = dotenv.get("GET_EMPLOYEE_ID_ENDPOINT")
-  private val getInternalDocsEndpoint = dotenv.get("GET_INTERNAL_DOCS_ENDPOINT")
-  private val postEmployeeDocEndpoint = dotenv.get("POST_EMPLOYEE_DOC_ENDPOINT")
-  private val postEmployeeProfileEndpoint = dotenv.get("POST_EMPLOYEE_PROFILE_ENDPOINT")
+  private val employeeCvEndpoint = dotenv.get("EMPLOYEE_CV_ENDPOINT")
+  private val employeeIdEndpoint = dotenv.get("EMPLOYEE_ID_ENDPOINT")
+  private val employeeProfileEndpoint = dotenv.get("EMPLOYEE_PROFILE_ENDPOINT")
+  private val employeeDocsEndpoint = dotenv.get("EMPLOYEE_DOCS_ENDPOINT")
+  private val internalDocsEndpoint = dotenv.get("INTERNAL_DOCS_ENDPOINT")
 
   def doStart(systemPrompt: Option[String], commandProps: CommandProps)
     (using context: ActorContext[Command]): Behavior[Command] =
@@ -45,22 +45,40 @@ object WorkerAgent extends Agent:
 
   private def callGetEmployeeCvTool(secret: String)(using context: ActorContext[Command]): Option[String] =
     val request = quickRequest
-      .get(uri"$serverBaseUrl:$serverPort/$getEmployeeCvEndpoint?secret=$secret")
-      .header("Content-Type", "application/json")
+      .get(uri"$serverBaseUrl:$serverPort/$employeeCvEndpoint?secret=$secret")
+      .header("Content-Type", "text/html")
     makeRequestWithRetries(request)
   end callGetEmployeeCvTool
 
   private def callGetEmployeeIdTool(secret: String)(using context: ActorContext[Command]): Option[String] =
     val request = quickRequest
-      .get(uri"$serverBaseUrl:$serverPort/$getEmployeeIdEndpoint?secret=$secret")
-      .header("Content-Type", "application/json")
+      .get(uri"$serverBaseUrl:$serverPort/$employeeIdEndpoint?secret=$secret")
+      .header("Content-Type", "text/html")
     makeRequestWithRetries(request)
   end callGetEmployeeIdTool
 
+  private def callPostEmployeeProfileTool(secret: String, profile: String)
+    (using context: ActorContext[Command]): Option[String] =
+    val request = quickRequest
+      .post(uri"$serverBaseUrl:$serverPort/$employeeProfileEndpoint?secret=$secret")
+      .header("Content-Type", "text/html")
+      .body(profile.trim)
+    makeRequestWithRetries(request)
+  end callPostEmployeeProfileTool
+
+  private def callPostEmployeeDocsTool(secret: String, filename: String, document: String)
+    (using context: ActorContext[Command]): Option[String] =
+    val request = quickRequest
+      .post(uri"$serverBaseUrl:$serverPort/$employeeDocsEndpoint?secret=$secret")
+      .header("Content-Type", "text/html")
+      .body(document.trim)
+    makeRequestWithRetries(request)
+  end callPostEmployeeDocsTool
+
   private def callGetInternalDocsTool(using context: ActorContext[Command]): List[Option[String]] =
     val request = quickRequest
-      .get(uri"$serverBaseUrl:$serverPort/$getInternalDocsEndpoint")
-      .header("Content-Type", "application/json")
+      .get(uri"$serverBaseUrl:$serverPort/$internalDocsEndpoint")
+      .header("Content-Type", "text/html")
     makeRequestWithRetries(request).map: docs =>
       docs
         .split("(?i)(?=<!DOCTYPE html>)")
@@ -68,25 +86,6 @@ object WorkerAgent extends Agent:
         .filter(_.nonEmpty)
         .map(Some(_))
     .getOrElse(Nil)
-
   end callGetInternalDocsTool
-
-  private def callPostEmployeeDocTool(secret: String, filename: String, document: String)
-    (using context: ActorContext[Command]): Option[String] =
-    val request = quickRequest
-      .post(uri"$serverBaseUrl:$serverPort/$postEmployeeDocEndpoint?secret=$secret&filename=$filename")
-      .header("Content-Type", "application/json")
-      .body(document.trim)
-    makeRequestWithRetries(request)
-  end callPostEmployeeDocTool
-
-  private def callPostEmployeeProfileTool(secret: String, profile: String)
-    (using context: ActorContext[Command]): Option[String] =
-    val request = quickRequest
-      .post(uri"$serverBaseUrl:$serverPort/$postEmployeeProfileEndpoint?secret=$secret")
-      .header("Content-Type", "application/json")
-      .body(profile.trim)
-    makeRequestWithRetries(request)
-  end callPostEmployeeProfileTool
 
 end WorkerAgent
